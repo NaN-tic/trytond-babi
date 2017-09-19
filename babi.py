dc11fc1ab617787531ec448582d06e160cfb8b50
Raimon Esteve <raimon@nan-tic.com>
2017-09-19 22:02:44 +0200

    FIX ident code From changeset-f9ea7e0f3861

--- babi.py
+++ babi.py
@@ -1621,7 +1621,7 @@ class OpenExecutionSelect(ModelView):
     report_readonly = fields.Boolean('Report Readonly')
     execution_readonly = fields.Boolean('Execution Readonly')
 
-     @classmethod
+    @classmethod
     def view_attributes(cls):
         return super(OpenChartStart, cls).view_attributes() + [(
             '/form/group[@id="labels"]', 'states',
