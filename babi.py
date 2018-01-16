11eb93602506e075a78c8a3021351347d252bf56
C?dric Krier <ced@b2ck.com>
2018-01-16 18:06:32 +0100

    Fix group value when copying menu

--- babi.py
+++ babi.py
@@ -793,10 +793,7 @@ class Report(ModelSQL, ModelView):
                 'parent': parent,
                 'babi_report': self.id,
                 'icon': 'tryton-executable',
-                'groups': [
-                    ('remove', [g.id for g in menu.groups]),
-                    ('add', [x.id for x in self.groups]),
-                    ],
+                'groups': [x.id for x in self.groups],
                 'babi_type': 'wizard',
                 'active': True,
                 })
@@ -814,10 +811,7 @@ class Report(ModelSQL, ModelView):
                 'parent': parent,
                 'babi_report': self.id,
                 'icon': 'tryton-executable',
-                'groups': [
-                    ('remove', [g.id for g in menu.groups]),
-                    ('add', [x.id for x in self.groups]),
-                    ],
+                'groups': [x.id for x in self.groups],
                 'babi_type': 'history',
                 'active': True,
                 })
