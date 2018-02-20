67127b9652735c738d2d3bb097e49ba557eaebb0
Raimon Esteve <raimon@nan-tic.com>
2018-02-20 14:52:12 +0100

    Add Report cell level option Start cell level that not has indentation task-033446

--- configuration.py
+++ configuration.py
@@ -8,5 +8,6 @@ __all__ = ['Configuration']
 class Configuration(ModelSingleton, ModelSQL, ModelView):
     'Business Intelligence Configuration'
     __name__ = 'babi.configuration'
-
     default_timeout = fields.Integer('Timeout (s)')
+    report_cell_level = fields.Integer('Cell Level',
+        help='Start cell level that not has indentation')
