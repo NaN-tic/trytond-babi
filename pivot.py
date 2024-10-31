24aae352f45500566215be6ba87a49ebce27d15f
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2024-10-31 12:41:07 +0100

    Add missing remove and change level functions in property field. Add new indicator of loading in pivot table. If the table is loading an icon and a text will show.
    Task #166342

--- pivot.py
+++ pivot.py
@@ -1,6 +1,6 @@
 from dominate.tags import (div, h1, p, pre, a, form, button, span, table, thead,
     tbody, tr, td, head, html, meta, link, title, script, h3, comment, select,
-    option, main, th)
+    option, main, th, style)
 from dominate.util import raw
 import logging
 from openpyxl import Workbook
@@ -46,6 +46,7 @@ CLOSE_ICON = raw('<svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-wi
 ORDER_ICON = raw('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" /></svg>')
 ORDER_ASC_ICON = raw('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0-3.75-3.75M17.25 21 21 17.25" /></svg>')
 ORDER_DESC_ICON = raw('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12" /></svg>')
+LOADING_SPINNER = raw('<svg aria-hidden="true" class="m-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg>')
 
 class Site(metaclass=PoolMeta):
     __name__ = 'www.site'
@@ -234,6 +235,10 @@ class Index(Component):
                     table_properties=self.table_properties)
 
             with div(cls="mt-8 flow-root"):
+                loading_div = div(id="loading-state", cls="htmx-indicator flex items-center")
+                loading_div.add(LOADING_SPINNER)
+                loading_div.add(p(_('Loading ...')))
+
                 with div(cls="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8"):
                     if show_error == True:
                         div(cls="text-center").add(p(_('You need to select at least one measure and one row or one column to show the table.'), cls="mt-1 text-sm text-gray-500"))
@@ -667,6 +672,8 @@ class PivotHeader(Component):
                 cube.rows.remove(self.field)
             case 'y':
                 cube.columns.remove(self.field)
+            case 'property':
+                cube.properties.remove(self.field)
             case 'measures':
                 #TODO: we need to handle the case when we have multiple
                 # measures for the same field.
@@ -706,6 +713,7 @@ class PivotHeader(Component):
         headers = {
             'x': 'rows',
             'y': 'columns',
+            'property': 'properties',
             'measures': 'measures',
             'order': 'order',
         }
@@ -823,7 +831,8 @@ class PivotTable(Component):
                                     table_name=self.table_name,
                                     table_properties=table_properties,
                                     render=False).url(),
-                            hx_trigger="click", hx_swap="outerHTML")
+                            hx_trigger="click", hx_swap="outerHTML",
+                            hx_indicator="#loading-state")
 
                     pivot_row.add(td(cell_value, cls="text-xs uppercase bg-gray-300 text-gray-900 px-6 py-3"))
 
