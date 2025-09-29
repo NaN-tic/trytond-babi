230bee53913350ee0ed612f30cd10eda43054e45
nan-tic-dev <79672209+nan-tic-dev@users.noreply.github.com>
2025-09-29 16:36:17 +0200

    Pivot Table report: table not found (#26)
    #179232

--- table.py
+++ table.py
@@ -2206,7 +2206,7 @@ class Pivot(ModelSQL, ModelView):
 
             for order in new.order:
                 order.sequence, order.order = mapping.get(rel.get(
-                    order.element))
+                    order.element), (None, 'asc'))
                 o_to_save.append(order)
 
         Order.save(o_to_save)
@@ -2482,8 +2482,11 @@ class PivotExcel(Report):
             cube = pivot.get_cube()
             cube.column_expansions = Cube.EXPAND_ALL
             cube.row_expansions = Cube.EXPAND_ALL
-            for row in cube.build():
-                ws.append([x.formatted(language, worksheet=True) for x in row])
+            try:
+                for row in cube.build():
+                    ws.append([x.formatted(language, worksheet=True) for x in row])
+            except psycopg2.errors.UndefinedTable:
+                continue
 
         if len(pivots) == 1:
             name = pivot.table.name
