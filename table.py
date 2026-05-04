d5aee6bb1b811143b97f7f90eb23c135aad4c24d
Raimon Esteve <raimonesteve@gmail.com>
2026-05-04 12:00:38 +0200

    Save last_warning_execution when compute warnings #186638

--- table.py
+++ table.py
@@ -1366,6 +1366,7 @@ class Table(DeactivableMixin, ModelSQL, ModelView):
 
         to_create = []
         self.last_warning_execution = datetime.now()
+        self.save()
         cursor = Transaction().connection.cursor()
         cursor.execute(query_full)
         query_last = cursor.fetchall()
