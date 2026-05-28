1bc479984c9bdcccd21a7426c84efd0db1eaa536
Albert Cervera i Areny <albert@nan-tic.com>
2026-05-28 16:57:54 +0200

    Use safeeval and implement parallel computation.
    safeeval makes the code about two times slower but we try to compensate that with:
    
    - Evaluating expressions in batch (all expressions for the same record are computed at once so evaluation returns a tuple of expression values), which is about twice as fast as the individual evaluation, so it takes more or less the same time it took before using safeeval
    - Allow executing in multiple processes. By experimenting I found that the default 10000 record batches we were using were in fact slower than 1000 in some cases, and it is in fact faster to use just 100, probably because of the related records that Tryton loads. That seems to be specially true when working on multiple processes (maybe because we're filling the CPU cache??).
    
    I left 2 as the default value for the number of processes. 4 scales pretty well too but I think it may bring too much unexpected load so it's better to configure that on a customer-by-customer basis.

--- babi_eval.py
+++ babi_eval.py
@@ -2,8 +2,10 @@
 # copyright notices and license terms.
 from decimal import Decimal
 import datetime
+from functools import lru_cache
 import math
 from dateutil.relativedelta import relativedelta
+from simpleeval import EvalWithCompoundTypes
 from trytond.pool import Pool
 from trytond.transaction import Transaction
 
@@ -56,31 +58,65 @@ def date(text):
     return datetime.datetime.strptime(year_month_day(text), '%Y-%m-%d').date()
 
 
-def babi_eval(expression, obj, convert_none='empty', digits=None, ttype=None):
-    objects = {
-        'o': obj,
-        'Pool': Pool,
-        'Transaction': Transaction,
-        'y': year,
-        'm': month,
-        'd': day,
-        'w': week,
-        'ym': year_month,
-        'ymd': year_month_day,
-        'date': date,
-        'int': int,
-        'float': float,
-        'sum': sum,
-        'min': min,
-        'max': max,
-        'now': datetime.datetime.now,
-        'today': datetime.date.today,
-        'relativedelta': relativedelta,
-        'math': math,
-        'Decimal': Decimal,
-        'str': str,
-        }
-    value = eval(expression, objects)
+def safe_getattr(obj, attr, default=None):
+    return getattr(obj, attr, default)
+
+
+def safe_hasattr(obj, attr):
+    return hasattr(obj, attr)
+
+
+def safe_isinstance(obj, classes):
+    return isinstance(obj, classes)
+
+
+def safe_pool(name):
+    return Pool().get(name)
+
+def safe_transaction():
+    return Transaction()
+
+
+FUNCTIONS = {
+    'y': year,
+    'm': month,
+    'd': day,
+    'w': week,
+    'ym': year_month,
+    'ymd': year_month_day,
+    'date': date,
+    'int': int,
+    'float': float,
+    'sum': sum,
+    'min': min,
+    'max': max,
+    'now': datetime.datetime.now,
+    'today': datetime.date.today,
+    'relativedelta': relativedelta,
+    'math': math,
+    'Decimal': Decimal,
+    'str': str,
+    'pool': safe_pool,
+    'transaction': safe_transaction,
+    'getattr': safe_getattr,
+    'hasattr': safe_hasattr,
+    'isinstance': safe_isinstance,
+    }
+
+
+@lru_cache(maxsize=1024)
+def _get_parsed_expression(expression):
+    return EvalWithCompoundTypes.parse(expression)
+
+
+@lru_cache(maxsize=256)
+def _get_batch_expression(expressions):
+    if len(expressions) == 1:
+        return f'({expressions[0]},)'
+    return '(' + ', '.join(expressions) + ')'
+
+
+def _normalize_value(value, convert_none='empty', digits=None, ttype=None):
     if (value is False or value is None):
         if convert_none == 'empty':
             # TODO: Make translatable
@@ -98,3 +134,41 @@ def babi_eval(expression, obj, convert_none='empty', digits=None, ttype=None):
         elif isinstance(value, float):
             value = round(value, digits)
     return value
+
+
+def _expand_batch_option(option, size):
+    if isinstance(option, (list, tuple)):
+        assert len(option) == size
+        return option
+    return [option] * size
+
+
+def babi_eval(expression, obj, convert_none='empty', digits=None, ttype=None):
+    return babi_eval_batch([expression], obj, convert_none=convert_none,
+        digits=digits, ttypes=ttype)[0]
+
+
+def babi_eval_batch(expressions, obj, convert_none='empty', digits=None,
+        ttypes=None):
+    expressions = tuple(expressions)
+    if not expressions:
+        return tuple()
+
+    evaluator = EvalWithCompoundTypes(
+        names={'o': obj}, functions=FUNCTIONS.copy())
+    if len(expressions) == 1:
+        value = evaluator.eval(expressions[0],
+            previously_parsed=_get_parsed_expression(expressions[0]))
+        return (_normalize_value(value, convert_none=convert_none,
+                digits=digits, ttype=ttypes),)
+
+    batch_expression = _get_batch_expression(expressions)
+    values = evaluator.eval(batch_expression,
+        previously_parsed=_get_parsed_expression(batch_expression))
+    convert_nones = _expand_batch_option(convert_none, len(expressions))
+    digits_list = _expand_batch_option(digits, len(expressions))
+    ttypes_list = _expand_batch_option(ttypes, len(expressions))
+    return tuple(_normalize_value(value, convert_none=convert_none_,
+            digits=digits_, ttype=ttype_)
+        for value, convert_none_, digits_, ttype_
+        in zip(values, convert_nones, digits_list, ttypes_list))
