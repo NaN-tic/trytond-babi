0d23baf5b9c42bb789cee4aec6f368b8f924c56a
Albert Cervera i Areny <albert@nan-tic.com>
2026-07-07 14:31:55 +0200

    Use xgettext shared translations

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ from configparser import ConfigParser
 MODULE = 'babi'
 PREFIX = 'nantic'
 MODULE2PREFIX = {
+    'xgettext': 'nantic',
     'html_report': 'nantic',
     'smtp': 'nantic',
     'widgets': 'nantic',
